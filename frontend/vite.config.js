e8708901fba1df8017018745670fc5b08ace1afe
Javed Khan <khanjaved.sk19@gmail.com>
2024-09-14 19:03:10 +0530

    fix: http mixed content error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL, // Accessing VITE_API_URL from the .env file
+        changeOrigin: true,
+        secure: false, // Disable SSL verification if you're using HTTP 
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
