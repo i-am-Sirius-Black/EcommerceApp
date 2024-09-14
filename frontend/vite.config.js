d436d71515fbe0b9f788c8100b31a00621e79e58
Javed Khan <khanjaved.sk19@gmail.com>
2024-09-14 21:00:23 +0530

    allowed all origins

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',  // Bind to all network interfaces
+    port: 5173,       // Your port number
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL, // Accessing VITE_API_URL from the .env file
+        target: process.env.VITE_API_URL || 'http://<your-backend-ec2-public-ip>',
         changeOrigin: true,
-        secure: false, // Disable SSL verification if you're using HTTP 
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
